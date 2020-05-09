1ddcf668006dfe271ab7d6a49452c6983d2181a9
Perks-of-Being-a-Cauliflower <wdctas@yahoo.com.au>
2020-05-09 12:40:39 +1000

    sell ticket menus

--- KIT305-607 Tutorial - Week 05 - iOS/Tutorial5/SecondViewController.swift
+++ KIT305-607 Tutorial - Week 05 - iOS/Tutorial5/SecondViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import SQLite3
 
-class SecondViewController: UIViewController {
+class SecondViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
 
     
     
@@ -26,9 +26,45 @@ class SecondViewController: UIViewController {
     var databaseFromPreviousView: SQLiteDatabase?
     //var cTicket = Ticket?()
     
+    @IBOutlet weak var ticketLabel: UILabel!
+    @IBOutlet weak var textInteractField: UITextField!
+    @IBOutlet weak var popUpMenu2: UIView!
+    @IBOutlet weak var numberToPurchaseField: UITextField!
+    @IBOutlet weak var pickerView: UIPickerView!
+    @IBOutlet weak var ticketIdentifier: UILabel!
+    
+    
+    
+    
     @IBOutlet var customerName: UITextField!
     @IBOutlet var customerPhone: UITextField!
     @IBOutlet var customerEmail: UITextField!
+    
+    private let tPicker = ["1", "2", "3", "4", "5", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20"]
+
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, attributedTitleForRow row: Int, forComponent component: Int) -> NSAttributedString? {
+            return NSAttributedString(string: tPicker[row], attributes: [NSAttributedString.Key.foregroundColor: UIColor.white])
+    
+    }
+    
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+            return tPicker.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        numberToPurchaseField.text = tPicker[row]
+        let basePrice = Double(ticket!.price)
+        let nOfTickets = Double(numberToPurchaseField.text!)!
+        let newCost = basePrice*nOfTickets
+        let totePrice:String = String(format:"$ %.1f", newCost)
+        totalCost.text = totePrice
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -59,12 +95,103 @@ class SecondViewController: UIViewController {
             //print("sees colour as: ", ticket?.colour as Any)
             colourBarOne.backgroundColor = colors[ticket!.colour]
             colourBarTwo.backgroundColor = colors[(ticket?.colour)!]
-            
+            ticketIdentifier.text = ticket!.iDLetter + " - " + ticket!.colour
             //ticketName.text = self.name
         }
         
         // Do any additional setup after loading the view.
     }
+    
+    
+    
+    
+    @IBAction func showInputMenu(_ sender: UITextField) {
+        customerName.isUserInteractionEnabled = false
+        customerPhone.isUserInteractionEnabled = false
+        customerEmail.isUserInteractionEnabled = false
+        numberToPurchaseField.isUserInteractionEnabled = false
+        customerEmail.resignFirstResponder()
+        customerPhone.resignFirstResponder()
+        customerName.resignFirstResponder()
+        numberToPurchaseField.resignFirstResponder()
+        print(sender.tag)
+            if sender.tag == 1 {
+                ticketLabel.text = "Name: "
+                textInteractField.isUserInteractionEnabled = true
+                textInteractField.isHidden = false
+                textInteractField.becomeFirstResponder()
+                let temp: String? = customerName.text
+                textInteractField.text = temp
+            } else if sender.tag == 2 {
+                ticketLabel.text = "Phone: "
+                textInteractField.keyboardType = UIKeyboardType.numberPad
+                textInteractField.isUserInteractionEnabled = true
+                textInteractField.isHidden = false
+                textInteractField.becomeFirstResponder()
+                let temp: String? = customerPhone.text
+                textInteractField.text = temp
+            } else if sender.tag == 3 {
+                ticketLabel.text = "Email: "
+                textInteractField.isUserInteractionEnabled = true
+                textInteractField.isHidden = false
+                textInteractField.becomeFirstResponder()
+                let temp: String? = customerEmail.text
+                textInteractField.text = temp
+            } else if sender.tag == 4 {
+                pickerView.isHidden = false
+                pickerView.becomeFirstResponder()
+                ticketLabel.text = "Tickets to Purchase: "
+            }
+            popUpMenu2.isHidden = false
+        }
+    
+    @IBAction func confirmDetailsButton(_ sender: Any) {
+        let returnField: String? = ticketLabel.text
+        textInteractField.isUserInteractionEnabled = false
+        customerEmail.isUserInteractionEnabled = true
+        customerPhone.isUserInteractionEnabled = true
+        customerName.isUserInteractionEnabled = true
+        numberToPurchaseField.isUserInteractionEnabled = true
+        if returnField == "Name: " {
+            textInteractField.isHidden = true
+            let newName: String? = textInteractField.text
+            customerName.text = newName
+        } else if returnField == "Phone: " {
+            textInteractField.isHidden = true
+            let newName: String? = textInteractField.text
+            customerPhone.text = newName
+            if let phoneN = Int(newName!) {
+                print("The user entered a value price of \(phoneN)")
+                
+            } else {
+                print("Not a valid number: \(newName!)")
+                customerPhone.text = ""
+            }
+            //print(customerPhone.text!)
+            textInteractField.keyboardType = UIKeyboardType.default
+        } else if returnField == "Email: " {
+            textInteractField.isHidden = true
+            let newName: String? = textInteractField.text
+            customerEmail.text = newName
+        } else if returnField == "Tickets to Purchase: " {
+            pickerView.isHidden = true
+        }
+        popUpMenu2.isHidden = true
+    }
+    
+    @IBAction func backButtonTapped(_ sender: UIButton) {
+            textInteractField.keyboardType = UIKeyboardType.default
+            customerEmail.isUserInteractionEnabled = true
+            customerPhone.isUserInteractionEnabled = true
+            customerName.isUserInteractionEnabled = true
+            numberToPurchaseField.isUserInteractionEnabled = true
+            textInteractField.isUserInteractionEnabled = false
+            textInteractField.isHidden = true
+            pickerView.isHidden = true
+            popUpMenu2.isHidden = true
+    }
+    
+    
     @IBAction func AddCustomer(_ sender: Any) {
         let database : SQLiteDatabase = SQLiteDatabase(databaseName:"MyDatabasesdfg")
         
